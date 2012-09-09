63d2eee44e53de86422af986b167a88e7cda7401
Mike McNeil <michael.r.mcneil@gmail.com>
2012-09-09 15:32:54 -0500

    Cleaned up documentation and api.

--- socket.js
+++ socket.js
@@ -2,102 +2,42 @@
 // for programatic communication with the Sails backend
 Mast.Socket =_.extend(
 {	
+	// Local reference to Socket.io object
 	io: window.io,	
+	
+	// The base url of the application
 	baseurl: window.location.origin,
+	
+	// Whether to connect automatically
 	autoconnect: true,
+	
+	// Map of entities and actions, by uri
+	routes: {},
+	
+	// Override backbone.sync when Socket object is instantiated
 	initialize: function() {
 		_.bindAll(this);
-		if (this.autoconnect) {
-			this.connect();
-		}
-				
-				
-		// Override Backbone.sync
-		Backbone.sync = function(method, model, options) {
-			var resp;
-					
-			switch (method) {
+		this.autoconnect && this.connect();
+		Backbone.sync = function(method, model, options) {						// Override Backbone.sync	
+			switch (method) {													// (reference: http://documentcloud.github.com/backbone/docs/backbone-localstorage.html)
 				case "read":
-					model.id ? Mast.Socket.find(model,options) : Mast.Socket.findAll(model,options);
+					model.id ? 
+						Mast.Socket.find(model,options) : 
+						Mast.Socket.findAll(model,options);
 					break;
 				case "create":
-					resp = Mast.Socket.create(model,options);
+					Mast.Socket.create(model,options);
 					break;
 				case "update":
-					resp = Mast.Socket.update(model,options);
+					Mast.Socket.update(model,options);
 					break;
 				case "delete":
-					resp = Mast.Socket.destroy(model,options);
+					Mast.Socket['delete'](model,options);
 					break;
 			}
 		};
 	},
-			
-	// CRUD methods for Backbone usage
-	// (reference: http://documentcloud.github.com/backbone/docs/backbone-localstorage.html)
-	create: function(model,options){
-		var url = (model.url() || model.collection.url) + "/create";
-		
-		this.send('message',_.extend({
-			url: url
-		},model.toJSON()),function (parsedResult) {
-			options && options.success && options.success(parsedResult);
-		});
-	},
-	find: function(model,options){
-		var url = model.url();
-		
-		// Remove trailing slash and add /update to url
-		url = url.replace(/\/*$/,'');
-		var id = +(url.match(/(\/[^\/]+)$/)[0].replace(/[^0-9]/,''));
-		url = url.replace(/(\/[^\/]+)$/,'/find');
-		
-		this.send('message',_.extend({
-			id: id,
-			url: url
-		},model.toJSON()),function (parsedResult) {
-			options && options.success && options.success(parsedResult);
-		});
-	},
-	findAll: function(collection,options){
-		var url = (collection.url) + "/findAll";
-		this.send('message',{
-			url: url
-		},function (parsedResult) {
-			options && options.success && options.success(parsedResult);
-		});
-	},
-	update: function(model,options){
-		var url = model.url();
-		
-		// Remove trailing slash and add /update to url
-		url = url.replace(/\/*$/,'');
-		var id = +(url.match(/(\/[^\/]+)$/)[0].replace(/[^0-9]/,''));
-		url = url.replace(/(\/[^\/]+)$/,'/update');
-		
-		this.send('message',_.extend({
-			id: id,
-			url: url
-		},model.toJSON()),function (parsedResult) {
-			options && options.success && options.success(parsedResult);
-		});
-	},
-	destroy: function(model,options){
-		var url = model.url();
-		
-		// Remove trailing slash and add /destroy to url
-		url = url.replace(/\/*$/,'');
-		var id = +(url.match(/(\/[^\/]+)$/)[0].replace(/[^0-9]/,''));
-		url = url.replace(/(\/[^\/]+)$/,'/destroy');
-		
-		this.send('message',_.extend({
-			id: id,
-			url: url
-		},model.toJSON()),function (parsedResult) {
-			options && options.success && options.success(parsedResult);
-		});
-	},
-
+	
 	// Connect to socket
 	connect: function(baseurl) {
 		if (!this.io) {
@@ -106,25 +46,18 @@ Mast.Socket =_.extend(
 			"client library (io) cannot be found!"
 			);
 		}
-		
 		if (this.connected) {
 			throw new Error(
 				"Can't connect to "+baseurl+ " because you're "+
 				"already connected to a socket @ " + this.baseurl+"!"
 				);
 		}
-					
 		this.baseurl = baseurl || this.baseurl;
-		
 		this._socket = this.io.connect(this.baseurl);
-		
-		// Listen for latest session data from server and update local store
-		Mast.Socket._socket.on('sessionUpdated',function(data) {
+		Mast.Socket._socket.on('sessionUpdated',function(data) {				// Listen for latest session data from server and update local store
 			Mast.Session = data;
 		});
-		
-		// Route server-sent comet events
-		Mast.Socket._socket.on('message',function(cometMessage) {
+		Mast.Socket._socket.on('message',function(cometMessage) {				// Route server-sent comet events
 			if (cometMessage.uri) {
 				Mast.Socket.route(cometMessage.uri,_.clone(cometMessage.data));
 			}
@@ -133,19 +66,13 @@ Mast.Socket =_.extend(
 				throw new Error('Unknown message received from server.');
 			}
 		});
-					
 		this.connected = true;
 	},
-	
-	// Map of entities and actions, by uri
-	routes: {},
 	
 	// Comet route handler
 	route: function (serverUri,serverData) {
 		var extractParams = Backbone.Router.prototype._extractParameters,
 			calculateRegex = Backbone.Router.prototype._routeToRegExp;
-			
-//		debug.debug("routing "+serverUri+"...");
 		_.each(Mast.Socket.routes,function(instances,routeUri) {
 			var regex=calculateRegex(routeUri);
 			if (serverUri.match(regex)) {
@@ -163,30 +90,80 @@ Mast.Socket =_.extend(
 		if (!Mast.Socket.routes[routeUri]) {
 			Mast.Socket.routes[routeUri] = [];
 		}
-		
 		Mast.Socket.routes[routeUri].push({
 			action: action,														// The function to trigger
 			context: context													// Component where the logic will run
 		})
 	},
-	
-	/**
-	 * If options is a function, treat it as a callback
-	 * Otherwise the "success" property will be treated as the callback
-	 */
-	request: function (url,data,options) {
-		// Remove trailing slash
-		url = url.replace(/\/*$/,'');
-		
-		this.send('message',{
-			url: url,
-			method: options.verb || options.method || 'GET',
-			data: data
-		},function (parsedResult) {
-			options && ((_.isFunction(options) && options || options.success) (parsedResult));
+			
+	// CRUD methods for Backbone usage
+	create: function(model,options){
+		var url = (model.url() || model.collection.url) + "/create";
+		this.post(url,model.toJSON(),function (parsedResult) {
+			options && options.success && options.success(parsedResult);
 		});
 	},
 	
+	find: function(model,options){
+		url = model.url().replace(/\/*$/,'');									// Remove trailing slash and add /update to url
+		var id = +(url.match(/(\/[^\/]+)$/)[0].replace(/[^0-9]/,''));
+		url = url.replace(/(\/[^\/]+)$/,'/find');
+		var data = _.extend({id:id},model.toJSON());							// Add id to data params
+		
+		this.get(url, data, function (parsedResult) {
+			options && options.success && options.success(parsedResult);
+		});
+	},
+	
+	findAll: function(collection,options){
+		var url = (collection.url) + "/findAll";
+		this.get(url, model.toJSON(), function (parsedResult) {
+			options && options.success && options.success(parsedResult);
+		});
+	},
+	
+	update: function(model,options){
+		var url = model.url().replace(/\/*$/,'');								// Remove trailing slash and add /update to url
+		var id = +(url.match(/(\/[^\/]+)$/)[0].replace(/[^0-9]/,''));
+		url = url.replace(/(\/[^\/]+)$/,'/update');
+		var data = _.extend({id:id},model.toJSON());							// Add id to data params
+		
+		this.put(url, data, function (parsedResult) {
+			options && options.success && options.success(parsedResult);
+		});
+	},
+	
+	'delete': function(model,options){
+		var url = model.url().replace(/\/*$/,'');								// Remove trailing slash and add /destroy to url
+		var id = +(url.match(/(\/[^\/]+)$/)[0].replace(/[^0-9]/,''));
+		url = url.replace(/(\/[^\/]+)$/,'/destroy');
+		var data = _.extend({id:id},model.toJSON());							// Add id to data params
+		
+		this['delete'](url,data,function (parsedResult) {
+			options && options.success && options.success(parsedResult);
+		});
+	},
+		
+	get: function (url,data,options) { this.request(url,data,options,'get') },
+	post: function (url,data,options) { this.request(url,data,options,'post') },
+	put: function (url,data,options) { this.request(url,data,options,'put') },
+	'delete': function (url,data,options) { this.request(url,data,options,'delete') },
+	
+	// Simulate an HTTP request to the backend
+	request: function (url,data,options, method) {
+		// Remove trailing slash
+		url = url.replace(/\/*$/,'');
+		
+		this._send('message',{
+			url: url,
+			data: data,
+			method: method || 'get'
+		},function (parsedResult) {
+			options && ((_.isFunction(options) ? 
+				options :														// If options is a function, treat it as a callback
+				options.success) (parsedResult));								// Otherwise the "success" property will be treated as the callback
+		});
+	},
 	
 	/**
 	 * Make a call to the server over the socket
@@ -194,7 +171,7 @@ Mast.Socket =_.extend(
 	 *   params:	data to pass with the request
 	 *   callback:	optional callback fired when server responds
 	 */
-	send: function (label,params,callback) {
+	_send: function (label,params,callback) {
 		Mast.Socket._socket.emit(label,JSON.stringify(params),function(result) {
 			try {
 				var parsedResult = JSON.parse(result);
@@ -202,9 +179,7 @@ Mast.Socket =_.extend(
 			catch (e) {
 				throw new Error("Server response could not be parsed:",result,e);
 			}
-			
-			// Call success callback if specified
-			callback && callback(parsedResult);
+			callback && callback(parsedResult);									// Call success callback if specified
 		})
 	}
 },Backbone.Events)
