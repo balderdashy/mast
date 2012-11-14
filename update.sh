ddf3816ed7749e3c621a89ae2c3bb9ac7fb2332d
Mike McNeil <michael.r.mcneil@gmail.com>
2012-11-14 11:00:15 -0600

    Updated update script and added rigging dependency for compilation.

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 if [ -z "$1" ]
 then
-        echo "Please specify the version as an argument.\n"
+  echo "Please specify the version as an argument.\n"
   exit 0
 else
   sed 's/{{version}}/'$1'/' package.json.template > package.json
+  #todo: use second argument (optional) as commit message and annotated tag message
+  git tag "$1"
   git commit -am "Bumped version to $1."
-  npm publish --force && git push
+  npm publish --force && git push --tags && git push
 fi
