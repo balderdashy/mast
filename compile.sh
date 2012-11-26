7e8a95f1259830519fe703d42c4bb2d81869904a
Mike McNeil <michael.r.mcneil@gmail.com>
2012-11-25 19:15:09 -0600

    Created new events.js file to bind global dom events.  Added a few dom events that are subscribable through the subscriptions has using the '$' prefix.  Also added support for the '%' prefix for susbcribing to Backbone triggers.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 # Build mast distributable
-./node_modules/rigging/bin/rigging.js lib/dependencies/socket.io.js lib/dependencies/jquery.min.js lib/dependencies/underscore.min.js lib/dependencies/underscore.string.min.js lib/dependencies/backbone.min.js lib/dependencies/json2.min.js lib/dependencies/logger.min.js lib/dependencies/outside.min.js lib/dependencies/pressFoo.min.js lib/mast.js lib/mixins.js lib/model.js lib/socket.js lib/pattern.js lib/component.js lib/tree.js lib/extend.js
+./node_modules/rigging/bin/rigging.js lib/dependencies/socket.io.js lib/dependencies/jquery.min.js lib/dependencies/underscore.min.js lib/dependencies/underscore.string.min.js lib/dependencies/backbone.min.js lib/dependencies/json2.min.js lib/dependencies/logger.min.js lib/dependencies/outside.min.js lib/dependencies/pressFoo.js lib/mast.js lib/mixins.js lib/model.js lib/socket.js lib/pattern.js lib/events.js lib/component.js lib/tree.js lib/extend.js
 
 # Copy it into the pwd
 cp .rigging_out/rigging.min.js mast.min.js
