b4f14d5a7063856e710d61a8b01397f34138ec90
Mike McNeil <michael.r.mcneil@gmail.com>
2013-05-25 12:28:12 -0500

    Clean up.

--- example/main.js
+++ example/main.js
@@ -1,8 +1,6 @@
 // You can set this up however you like, we're just using require here to try it out.
 require([
 	'components/App',
-	'models/Me',
-	'models/Todo',
 	'templates/Settings.html',
 	'templates/TodoList.html'
 	], function () {
--- example/models/Me.js
+++
@@ -1,8 +0,0 @@
-define([], function () {
-	Mast.Collection.extend({
-		name	: 'Me',
-		
-		fetch	: 'get /session',
-		save	: 'put /session'
-	});
-});
