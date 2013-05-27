cf15ad0c91c224139416d45f454dc6d5113ba402
Mike McNeil <michael.r.mcneil@gmail.com>
2013-05-27 16:03:41 -0500

    Build script and compiled version.

---
+++ build.sh
@@ -0,0 +1,39 @@
+touch mast.dev.js
+
+# Core dependencies
+cat "lib/deps/$.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/deps/_.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/deps/Backbone.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/deps/async.js" >> mast.dev.js
+echo '' >> mast.dev.js
+
+# Core
+cat "lib/mast.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/Util.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/define.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/Component.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/Region.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/Data.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/Router.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/Delegator.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/Comet.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/ready.js" >> mast.dev.js
+echo '' >> mast.dev.js
+cat "lib/raise.js" >> mast.dev.js
+echo '' >> mast.dev.js
+
+# Disable debug mode
+echo 'Mast.debug = false;' >> mast.dev.js
+echo '' >> mast.dev.js
