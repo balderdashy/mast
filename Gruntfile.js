c84ee635c194a6a01d25cd3e3076bc5315d8263d
Gabe Hernandez <ghernandez345@gmail.com>
2013-10-10 16:05:23 -0500

    set up grunt to create dev and prod versions of mast.

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,48 +27,45 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		concat: {
-
-			// Concatenate the source files
-			// into a single `framework.dev.js` file for the example
-			dist: {
-				src: srcFiles,
-				dest: 'example/dependencies/framework.dev.js'
-			}
-		},
-
 		uglify: {
 			options: {
 				report: 'min',
 				preserveComments: false
 			},
 			files: {
-				src: 'framework.dev.js',
-				dest: 'framework.min.js'
+				src: './dist/mast.min.js',
+				dest: './dist/mast.min.js',
 			}
 		},
 
 		watchify: {
-			options: {
-				debug: true
-			},
 			dev: {
-        src: './lib/src/build.js',
-        dest: './lib/mast.js'
-      }
+				options: {
+					debug: true
+				},
+				src: './lib/src/build.js',
+				dest: './dist/mast.dev.js'
+			},
+
+			prod: {
+				options: {
+					debug: false
+				},
+				src: './lib/src/build.js',
+				dest: './dist/mast.min.js'
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-watchify');
 
 	// It's so close!
 	grunt.registerTask('default', [
-		'build:dev'
-		// 'build:production'
+		'build:dev',
+		'build:prod'
 	]);
 
-	grunt.registerTask('build:dev', ['watchify']);
-	grunt.registerTask('build:production', ['concat', 'uglify']);
+	grunt.registerTask('build:dev', ['watchify:dev']);
+	grunt.registerTask('build:prod', ['watchify:prod', 'uglify']);
 };
