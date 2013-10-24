2667986930233890c5b43dde848fda57200b8ce7
Gabe Hernandez <ghernandez345@gmail.com>
2013-10-24 14:18:48 -0500

    write example of collection event hooks

---
+++ example/components/About.js
@@ -0,0 +1,68 @@
+/**
+ * About
+ *
+ * The UI logic controlling the about view.
+ * We are using collection binding methods here.
+ */
+
+FRAMEWORK.define('About', function () {
+	return {
+
+
+		/**
+		 * Lifecycle callbacks
+		 */
+		beforeRender: function(cb) {
+			this.collection = new Backbone.Collection([
+				{name: 'Sparky'},
+				{name: 'Ruffus'},
+				{name: 'Buddy'}
+			]);
+
+			// Be sure to call callback at the end of beforeRender
+			cb();
+		},
+
+		afterRender: function() {
+			this.renderDogs();
+		},
+
+
+		/**
+		 * Event listeners
+		 */
+		events: {
+			'click .add-dog': 'addDog'
+		},
+
+
+		/**
+		 * Component Methods
+		 */
+		addDog: function() {
+			var dogName = this.$('#dogInput').val().trim();
+
+			if (dogName)
+				this.collection.add({name: dogName});
+
+			this.$('#dogInput').val('');
+		},
+
+		// Render the dog items by appending a DogItem component to the dog list region.
+		// Each DogItem component gets instantiated with a passed in dog model.
+		renderDogs: function() {
+			this.collection.each(function(dog) {
+				this.dogList.append('DogItem', {model: dog});
+			}, this);
+		},
+
+
+
+		/**
+		 * Collection bindings
+		 */
+		afterAdd: function(model, collection, options) {
+			this.dogList.append('DogItem', {model: model});
+		}
+	};
+});
