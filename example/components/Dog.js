bf984bc3633387a6677bc68bde1e804ca52d6dba
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-08 03:39:56 -0600

    Class name data bindings work (dynamic parameters)

--- example/components/Dog.js
+++ example/components/Dog.js
@@ -8,9 +8,27 @@
 TEST.define('Dog', function () {
 	return {
 
-		events: {
-			// Toggle highlighting on this dog's $el
-			click: '! .highlighted.'
+		// This direct DOM manipulation is no longer necessary-- HTML data bindings can be used instead.
+		// Both approaches will continue to be supported.
+		//
+		// events: {
+		// 	click: '! .highlighted.'
+		// }
+
+
+		// Instead:
+		// Toggle highlighting on this dog's $el
+		click: function () {
+			if (this.model.get('selected')) {
+				this.model.set({selected: false});
+			}
+			else {
+				this.model.set({selected: true});
+			}
 		}
+
+
+		// Eventually- go back to shorthand, but this time for changing the model:
+		// click: '! @selected'
 	};
 });
