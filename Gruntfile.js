a0d6889502f51358164945528066f34d0c79efa0
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-02 18:48:38 -0600

    Flatten dir structure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,5 @@
 module.exports = function(grunt) {
 
-	// Order of the source files that we want to concatenate
-	var srcFiles = [
-		// Dependencies
-		'lib/deps/$.js',
-		'lib/deps/_.js',
-		'lib/deps/Backbone.js',
-		'lib/touch.js',
-		'lib/deps/async.js',
-
-		// Core
-		'lib/Framework.js',
-		'lib/Logger.js',
-		'lib/Util.js',
-		'lib/define.js',
-		'lib/Component.js',
-		'lib/Region.js',
-		'lib/Data.js',
-		'lib/Router.js',
-		'lib/ready.js',
-		'lib/raise.js'
-	];
-
-
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
